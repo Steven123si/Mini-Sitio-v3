3803c074fb6f020f14b019ecf00e2def25b73eb1
Steven123si <stevennt4@gmail.com>
2025-11-06 15:17:48 -0500

    commit de archivos

---
+++ script.js
@@ -0,0 +1,24 @@
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('show');
+});
+
+const form = document.getElementById('form-suscripcion');
+const emailInput = document.getElementById('email');
+const mensaje = document.getElementById('mensaje');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const correo = emailInput.value.trim();
+
+  if (correo === "") {
+    mensaje.textContent = "Por favor, ingresa tu correo electrónico.";
+    mensaje.style.color = "red";
+  } else {
+    mensaje.textContent = "Gracias por suscribirte 🎉";
+    mensaje.style.color = "green";
+    form.reset();
+  }
+});
